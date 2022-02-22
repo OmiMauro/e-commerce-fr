8aed83c1bdf9617c7df57d6fc50ba44485b80f43
Manu Ominuka <ominuka.mauro@gmail.com>
2022-02-22 15:56:10 -0300

    Add Carousel for view slides

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
 import { Navbar } from './component/Navbar'
 import { Home } from './component/Home'
+import { Carousel } from './component/Carousel'
 function App() {
   return (
     <>
       <Navbar />
+      <Carousel />
       <Home />
+
     </>
   )
 }
