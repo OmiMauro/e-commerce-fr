af93a897bf6b622d965fe108bf0fb8d9e580f841
Manu Ominuka <ominuka.mauro@gmail.com>
2022-02-23 09:20:27 -0300

    Add footer to page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css'
 import { Navbar } from './component/Navbar'
 import { Home } from './component/Home'
 import { Carousel } from './component/Carousel'
+import { Footer } from './component/Footer'
 function App() {
   return (
     <>
       <Navbar />
       <Carousel />
       <Home />
-
+      <Footer />
     </>
   )
 }
