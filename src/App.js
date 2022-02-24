ed85e0b7720d822e5c7a412a94a4054b71771876
Manu Ominuka <ominuka.mauro@gmail.com>
2022-02-24 16:52:03 -0300

    Add product by id in routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom'
 import { Products } from './component/Products'
 import { Contacts } from './component/Contacts'
 import { AboutUs } from './component/About-Us'
+import { Product } from './component/Product'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' exact element={<Home />} />
         <Route path='/products' exact element={<Products />} />
+        <Route path='/product/:id' exact element={<Product />} />
         <Route path='/contacts' exact element={<Contacts />} />
         <Route path='/about' exact element={<AboutUs />} />
       </Routes>
