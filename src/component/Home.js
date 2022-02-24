d8f28a405edc9ccc30c8eda582787121a1b1544a
Manu Ominuka <ominuka.mauro@gmail.com>
2022-02-24 09:13:09 -0300

    Add products and import into Home.

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import { Products } from './Products'
 
 const Home = () => {
   return (
     <div className="hero">
-      <div class="card bg-dark text-white border-0">
-        <img src="assets/bg.jpg" class="card-img" alt="..." height='550px' />
-        <div class="card-img-overlay d-flex align-items-center">
+      <div className="card bg-dark text-white border-0">
+        <img src="assets/bg.jpg" className="card-img" alt="..." height='550px' />
+        <div className="card-img-overlay d-flex align-items-center">
           <div className="container">
 
-            <h5 class="card-title display-3 fw-bolder">New Seasons arrivals</h5>
-            <p class="card-text lead fs-2">
+            <h5 className="card-title display-3 fw-bolder">New Seasons arrivals</h5>
+            <p className="card-text lead fs-2">
               Chequea todas las tendencias
             </p>
-            <p class="card-text">Last updated 3 mins ago</p>
+            <p className="card-text">Last updated 3 mins ago</p>
           </div>
 
         </div>
       </div>
+      <Products />
+
     </div>
   )
 }
---
+++ src/component/Products.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Skeleton from 'react-loading-skeleton';
+
+const Products = () => {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [filter, setFilter] = useState(data)
+
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true)
+      const products = await axios.get('https://fakestoreapi.com/products?limit=15')
+      setData(products.data)
+      setFilter(products.data)
+      setLoading(false)
+    }
+    getProducts()
+  }, [])
+  const Loading = () => {
+    return (
+      <div className="col-md-3">
+        <Skeleton height={350} />
+      </div>
+    )
+  }
+  const filterProduct = category => {
+    const updatedList = data.filter(item => item.category === category)
+    setFilter(updatedList)
+  }
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center my-5 py-5">
+          <button className="btn btn-outline-dark me-2" onClick={() => setFilter(data)}>All Products</button>
+          <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('men\'s clothing')}>Mens</button>
+          <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('women\'s clothing')}>Women's</button>
+          <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('jewelery')} >Jewelery</button>
+          <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('electronics')}>Electronic</button>
+
+        </div>
+        {
+          filter.map(product => {
+            return (
+              <div className="col-md-3 ">
+                <div class="card h-100 text-center py-4">
+                  <img src={product.image} class="card-img-top" alt={product.title} height='250px' />
+                  <div class="card-body">
+                    <h5 class="card-title mb-0">{product.title.substring(0, 12)} ...</h5>
+                    <p class="card-text lead fw-bolder">
+                      $ {product.price}
+                    </p>
+                  </div>
+                  <a href="#" className='btn btn-outline-dark'>Comprar ahora</a>
+                </div>
+              </div>
+            )
+          })
+        }
+
+      </>
+
+    )
+  }
+  return (
+    <div className="container my-5 py-5">
+      <div className="row">
+        <h1 className="col-12 text-center display-6 fw-bolder text-uppercase">Ultimos productos creados</h1>
+        <hr />
+      </div>
+      <div className="row justify-content-center">
+        {
+          loading ? <Loading /> : <ShowProducts />
+        }
+      </div>
+    </div>
+  )
+}
+
+export { Products };
